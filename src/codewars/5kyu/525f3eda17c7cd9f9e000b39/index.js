fd1ebd47743c2143ee155305afa8c86fc723fed8
dastasoft <dastasoft@gmail.com>
2021-01-21 00:18:37 +0100

    improved calculating with functions response

--- src/codewars/5kyu/525f3eda17c7cd9f9e000b39/index.js
+++ src/codewars/5kyu/525f3eda17c7cd9f9e000b39/index.js
@@ -1,37 +1,18 @@
-function makeIt(operation, num) {
-  return operation ? operation(num) : num
-}
-
-function zero(operation) {
-  return makeIt(operation, 0)
-}
-function one(operation) {
-  return makeIt(operation, 1)
-}
-function two(operation) {
-  return makeIt(operation, 2)
-}
-function three(operation) {
-  return makeIt(operation, 3)
-}
-function four(operation) {
-  return makeIt(operation, 4)
-}
-function five(operation) {
-  return makeIt(operation, 5)
-}
-function six(operation) {
-  return makeIt(operation, 6)
-}
-function seven(operation) {
-  return makeIt(operation, 7)
-}
-function eight(operation) {
-  return makeIt(operation, 8)
-}
-function nine(operation) {
-  return makeIt(operation, 9)
-}
+const numberFunctions = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+].reduce((accumulator, name, index) => {
+  accumulator[name] = operation => (operation ? operation(index) : index)
+  return accumulator
+}, {})
 
 function plus(rightOperand) {
   return leftOperand => leftOperand + rightOperand
@@ -46,17 +27,10 @@ function dividedBy(rightOperand) {
   return leftOperand => Math.trunc(leftOperand / rightOperand)
 }
 
+console.log(numberFunctions)
+
 module.exports = {
-  zero,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
+  ...numberFunctions,
   plus,
   minus,
   times,
